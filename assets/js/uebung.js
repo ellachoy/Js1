0a751db40a6b01d11ea370e00f3527222a17cb2c
Ella Choy <egal21de@gmail.com>
2021-08-15 20:47:54 +0200

    jsuebung

---
+++ assets/js/uebung.js
@@ -0,0 +1,14 @@
+// document.write("hello world");
+// document.write(1+1);
+function BgNight(){
+    document.querySelector('body').style.backgroundColor="black";
+    document.getElementById("Night").style.color="red";
+    // document.querySelector('body').style.color="red";
+
+}
+function BgDay(){
+    document.querySelector('body').style.backgroundColor="green";
+     document.getElementById("Day").style.color="yellow";
+    // document.querySelector('body').style.color="yellow";
+ }
+ document.write(1===1);
